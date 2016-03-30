debefdd2ba049f2b74d738f89e2752344ec57221
Evan Xd <itoyxd@gmail.com>
2016-03-30 18:40:33 +0800

    Add config variables

--- photon-station.ino
+++ photon-station.ino
@@ -1,11 +1,15 @@
 #include "application.h"
 #include "HttpClient/HttpClient.h"
 
+String SERVER_ADDRESS = "127.0.0.1";
+int SERVER_PORT = 8080;
+String SENSOR_KEY = "sensorKey";
+
 HttpClient http;
 http_header_t headers[] = {
   { "Content-Type", "application/x-www-form-urlencoded" },
   { "Accept" , "application/json" },
-  { NULL, NULL } // NOTE: Always terminate headers will NULL
+  { NULL, NULL } // NOTE: Always add the NULL object in the end of the headers.
 };
 http_request_t request;
 http_response_t response;
@@ -34,20 +38,20 @@ void loop() {
     if (index == 4 || index == 6 || index == 8 || index == 10 || index == 12 || index == 14) {
       previousValue = value;
     }
-    else if (index == 5){
+    else if (index == 5) {
       pm10 = 256 * previousValue + value;
       Serial.print("{ ");
       Serial.print("\"pm10\": ");
       Serial.print(pm10);
       Serial.print(", ");
     }
-    else if (index == 7){
+    else if (index == 7) {
       pm25 = 256 * previousValue + value;
       Serial.print("\"pm25\": ");
       Serial.print(pm25);
       Serial.print(", ");
     }
-    else if (index == 9){
+    else if (index == 9) {
       pm100 = 256 * previousValue + value;
       Serial.print("\"pm100\": ");
       Serial.print(pm100);
@@ -59,11 +63,11 @@ void loop() {
   while(Serial1.available()) Serial1.read();
   Serial.println(" }");
 
-  if (pm10 != 0 || pm25 != 0 || pm100 != 0) {
+  if (pm10 != 0 && pm25 != 0 && pm100 != 0) {
     Serial.println("Application>\tStart of Loop.");
-    request.hostname = "127.0.0.1";
-    request.port = 8080;
-    request.path = "/sensors/56f9dabaf95396dd045eb025/data";
+    request.hostname = SERVER_ADDRESS;
+    request.port = SERVER_PORT;
+    request.path = "/sensors/" + SENSOR_KEY + "/data";
     String pm25String;
     pm25String = pm25;
     request.body = "pm25Index=" + pm25String;
