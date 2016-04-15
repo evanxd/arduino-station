02efb5ed5b74f53d2a3f549620ecc0cf241d853a
Evan Xd <itoyxd@gmail.com>
2016-04-15 22:47:04 +0800

    Fix nits

--- station/photon/photon-station.ino
+++ station/photon/photon-station.ino
@@ -35,23 +35,21 @@ void loop() {
       break;
     }
 
-    if (index == 4 || index == 6 || index == 8 || index == 10 || index == 12 || index == 14) {
+    if (index == 4 || index == 6 || index == 8 ||
+        index == 10 || index == 12 || index == 14) {
       previousValue = value;
-    }
-    else if (index == 5) {
+    } else if (index == 5) {
       pm10 = 256 * previousValue + value;
       Serial.print("{ ");
       Serial.print("\"pm10\": ");
       Serial.print(pm10);
       Serial.print(", ");
-    }
-    else if (index == 7) {
+    } else if (index == 7) {
       pm25 = 256 * previousValue + value;
       Serial.print("\"pm25\": ");
       Serial.print(pm25);
       Serial.print(", ");
-    }
-    else if (index == 9) {
+    } else if (index == 9) {
       pm100 = 256 * previousValue + value;
       Serial.print("\"pm100\": ");
       Serial.print(pm100);
